af401082ad2c3b02e6e04d64365c2a7e4a4993ce
harilal766 <harilalsunil2@gmai.com>
2025-02-13 00:45:52 +0530

    added rows

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ let drawingBoard = document.getElementById("drawingBoard");
 
 function board(){
     let pixelCount = 0;
-    let limit = 9;
+    let limit = 16;
     // column
 
     for (let col=1; col <= limit; col++){
-        for (let row=1; row <= limit; row++){
-            pixelCount += 1;
-            if (pixelCount % limit ===0) {
-                console.log(pixelCount);
-            }
-        }
+        let rowDiv = document.createElement("div"); rowDiv.id =`row`;
+        drawingBoard.appendChild(rowDiv);
     }
 }
 board();
